5af84cc24448beacd7ef875700324328585866ed
Cristobal Zamorano Astudillo <cristobal.zamorano@berkeley.edu>
2020-11-24 12:06:14 -0800

    before oh and selenium tut

--- job_scrapper.py
+++ job_scrapper.py
@@ -5,4 +5,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 #specify driver path
 DRIVER_PATH = '/Users/cristobalza/Desktop/chromedriver'
-driver = webdriver.Chrome(executable_path = DRIVER_PATH)
+browser = webdriver.Chrome(executable_path = DRIVER_PATH)
+
+browser.get('https://cristobalza.com')
+print(browser.title)
+browser.quit()
+
+# initial_search_button = browser.find_element_by_xpath('//*[@id=”whatWhereFormId”]/div[3]/button')
+# initial_search_button.click()
