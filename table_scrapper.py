83d0ae2b31d99bec0c817f657bc182a6edd6758f
Cristobal Zamorano Astudillo <cristobal.zamorano@berkeley.edu>
2021-01-14 15:59:30 -0800

    added table scrapper

---
+++ table_scrapper.py
@@ -0,0 +1,61 @@
+import csv
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re
+
+url='https://www.numbeo.com/cost-of-living/region_rankings.jsp?title=2020-mid&region=019'
+#Create a handle, page, to handle the contents of the website
+
+# Make a GET request to fetch the raw HTML content
+html_content = requests.get(url).text
+
+# Parse HTML code for the entire site
+soup = BeautifulSoup(html_content, "lxml")
+# print(soup.prettify()) # print the parsed data of html
+
+# On site there are 3 tables with the class "wikitable"
+# The following line will generate a list of HTML content for each table
+table = soup.find_all("table", attrs={"id": "t2"})
+print("Number of tables on site: ",len(table))
+
+# Select table of interest
+table1 = table[0]
+# the head will form our column names
+body = table1.find_all("tr")
+# Head values (Column names) are the first items of the body list
+head = body[0] # 0th item is the header row
+body_rows = body[1:] # All other items becomes the rest of the rows
+
+# Lets now iterate through the head HTML code and make list of clean headings
+
+# Declare empty list to keep Columns names
+headings = []
+for item in head.find_all("th"): # loop through all th elements
+    # convert the th elements to text and strip "\n"
+    item = (item.text).rstrip("\n")
+    # append the clean column name to headings
+    headings.append(item)
+# print(headings)
+
+# Next is now to loop though the rest of the rows
+
+#print(body_rows[0])
+all_rows = [] # will be a list for list for all rows
+for row_num in range(len(body_rows)): # A row at a time
+    row = [] # this will old entries for one row
+    for row_item in body_rows[row_num].find_all("td"): #loop through all row entries
+        # row_item.text removes the tags from the entries
+        # the following regex is to remove \xa0 and \n and comma from row_item.text
+        # xa0 encodes the flag, \n is the newline and comma separates thousands in numbers
+        aa = re.sub("(\xa0)|(\n)|,","",row_item.text)
+        #append aa to row - note one row entry is being appended
+        row.append(aa)
+    # append one row to all_rows
+    all_rows.append(row)
+
+# We can now use the data on all_rowsa and headings to make a table
+# all_rows becomes our data and headings the column names
+df = pd.DataFrame(data=all_rows,columns=headings)
+
+df.to_csv('./data/cost_of_living_mid_2020.csv')
